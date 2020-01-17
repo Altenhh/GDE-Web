0126166189bdf73283b12b2a16f5c61aca2f7533
Alten <altenyt@gmail.com>
2020-01-17 15:46:25 -0500

    main header tab control

---
+++ GDEdit Web/GDE.Web/Data/TabControlObject.cs
@@ -0,0 +1,18 @@
+﻿namespace GDE.Web.Data
+{
+    public class TabControlObject
+    {
+        public string String { get; set; }
+
+        public bool Selected { get; set; }
+
+        public bool Disabled { get; set; }
+
+        public TabControlObject(string String, bool selected = false, bool disabled = false)
+        {
+            this.String = String;
+            Selected = selected;
+            Disabled = disabled;
+        }
+    }
+}
