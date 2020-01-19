65b495a713385be24e78661d9c478f8a739c6ca7
Alten <altenyt@gmail.com>
2020-01-18 22:25:18 -0500

    project cards

--- GDEdit Web/GDE.Web/Data/Projects/ProjectData.cs
+++ GDEdit Web/GDE.Web/Data/Projects/ProjectData.cs
@@ -2,12 +2,16 @@
 {
     public class ProjectData : ProjectDataMini
     {
+        /// <summary>How many issues the project has.</summary>
         public int Issues { get; set; }
 
+        /// <summary>How many Pull Requests the project has (<seealso cref="https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/about-pull-requests"/>).</summary>
         public int PullRequests { get; set; }
 
+        /// <summary>How many forks the project has.</summary>
         public int Forks { get; set; }
 
+        /// <summary>What License the project is using</summary>
         public string License { get; set; }
     }
 }
--- GDEdit Web/GDE.Web/Data/Projects/ProjectDataMini.cs
+++ GDEdit Web/GDE.Web/Data/Projects/ProjectDataMini.cs
@@ -1,19 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GDE.Web.Data.Projects
 {
     public class ProjectDataMini
     {
+        /// <summary>Name of the project.</summary>
         public string Name { get; set; }
+        
+        /// <summary>List of owners that are handling the project.</summary>
+        public List<string> Authors { get; set; }
 
+        /// <summary>Background URL of the project (OPTIONAL).</summary>
         public string BackgroundURL { get; set; }
 
+        /// <summary>Logo URL of the project.</summary>
         public string LogoURL { get; set; }
 
+        /// <summary>What language the project was written in.</summary>
         public string MainLanguage { get; set; }
 
+        /// <summary>Acronym of the language written in (i.e c# -> cs, visual basic -> vb).</summary>
+        public string LanguageAcronym { get; set; }
+
+        /// <summary>Last time the project has been updated.</summary>
+        /// <returns><see cref="DateTime.Now"/></returns>
         public DateTime LastUpdated { get; set; } = DateTime.Now;
 
+        /// <summary>How many stars the project currently has.</summary>
         public int Stars { get; set; }
     }
 }
