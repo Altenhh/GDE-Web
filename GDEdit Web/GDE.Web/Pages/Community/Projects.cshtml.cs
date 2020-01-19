bc66cf684a34d407df6e2804418f55c1f0d96af8
Alten <altenyt@gmail.com>
2020-01-18 20:55:11 -0500

    Initial projects page

---
+++ GDEdit Web/GDE.Web/Pages/Community/Projects.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using GDE.Web.Data;
+using GDE.Web.Data.Projects;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GDE.Web.Pages.Community
+{
+    public class ProjectsModel : PageModel
+    {
+        public List<ProjectData> Data { get; set; } = TempDatabase.ProjectDatas;
+        
+        public void OnGet()
+        {
+            GlobalVariables.CurrentSection = LinkItems.community;
+        }
+    }
+}
