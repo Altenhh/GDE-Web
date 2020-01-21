b76c33e033ce880a2763ea7dea7c86a0332d74fd
Alten <altenyt@gmail.com>
2020-01-20 20:08:07 -0500

    documentation

--- GDEdit Web/GDE.Web/Entities/User.cs
+++ GDEdit Web/GDE.Web/Entities/User.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        
+        // make sure this is kept safe at all times
         public string Token { get; set; }
     }
 }
